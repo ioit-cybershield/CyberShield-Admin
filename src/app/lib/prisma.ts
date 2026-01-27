bd2f7914ab9e08f2d7ca588e697458fe594bfa8e
pvnjdv <p8975306526@gmail.com>
2026-01-27 11:35:02 +0530

    prisma: setup

---
+++ src/app/lib/prisma.ts
@@ -0,0 +1,12 @@
+// lib/prisma.ts
+import { Pool, neonConfig } from "@neondatabase/serverless";
+import { PrismaNeon } from "@prisma/adapter-neon";
+import { PrismaClient } from "@/generated/prisma/client";
+
+const connectionString = process.env.DATABASE_URL!;
+const pool = new Pool({ connectionString });
+const adapter = new PrismaNeon(pool);
+
+const prisma = new PrismaClient({ adapter });
+
+export default prisma;
